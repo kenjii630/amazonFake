602b2ba10e1796b7e7df84a58cd69cfd8f1f1c23
kenjii630 <ratanak.vichea88@gmail.com>
2022-06-18 12:23:11 +0700

    Amazonfake

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon Fake</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
